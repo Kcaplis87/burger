0f84e0577594b5f3ce04b9e8f43f3716f07e5a36
Kyle Caplos <kylemcaplis@hotmail.com>
2019-03-05 10:30:10 -0500

    forgot to push as i go

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger) VALUES ("Mushroom and Swiss");
+INSERT INTO burgers (burger) VALUES ("Jalapeno Cheddar");
+INSERT INTO burgers (burger) VAlUES ("Beef Brisket and Beer Cheese");
+INSERT INTO burgers (burger) VALUES ("American Cheese and Bacon");
+INSERT INTO burgers (burger) VAlUES ("Crispy Onion Strings and BBQ Sauce");
